ddf6cd5c84895b39e7c85681a9d83215cbd1cc47
10001104 <maciejw@atena.pl>
2025-01-22 20:29:36 +0100

    7

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const express = require('express');
         }
     });
 
-    const port = 8080;
+    const port = 80;
     app.listen(port, () => {
         console.log(`Access code service listening on port !` + port);
     });
